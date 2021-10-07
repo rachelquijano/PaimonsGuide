4186ffd40f5bee5daaa49781f60744267fd2e3a9
Rachel Quijano <rquij005@fiu.edu>
2021-10-07 00:52:00 -0400

    Created home layout

--- app/src/main/java/com/rachelquijano/paimonsguide/MainActivity.java
+++ app/src/main/java/com/rachelquijano/paimonsguide/MainActivity.java
@@ -1,10 +1,14 @@
 package com.rachelquijano.paimonsguide;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,15 +18,28 @@ import com.rachelquijano.paimonsguide.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    public static final String TAG = "MainActivity";
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                fragment = new HomeFragment();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
 
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
-
 }
